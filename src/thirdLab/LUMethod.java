16b752d3ee6bf51367f142a6d58646748a3e4536
Deimoz <53293198+Deimoz@users.noreply.github.com>
2021-05-06 22:02:40 +0300

    Добавлен класс LUMethod (без кода) для решения СЛАУ

---
+++ src/thirdLab/LUMethod.java
@@ -0,0 +1,17 @@
+package thirdLab;
+
+import thirdLab.matrix.Matrix;
+
+public class LUMethod {
+    public double[] solve(Matrix m, double[] b) {
+
+    }
+
+    private double[] directWalk(LUMatrix LU, double[] b) {
+
+    }
+
+    private double[] reverseWalk(LUMatrix LU, double[] y) {
+
+    }
+}
