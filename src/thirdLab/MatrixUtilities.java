f656e5449a83ee58a1580df4dbaf32c0d17a7ec8
Son-gxisto <nikitoc17.09@gmail.com>
2021-05-06 23:13:02 +0300

    added point 2 and 3

--- src/thirdLab/MatrixUtilities.java
+++ src/thirdLab/MatrixUtilities.java
@@ -3,6 +3,7 @@ package thirdLab;
 import thirdLab.matrix.Matrix;
 import thirdLab.matrix.ProfileMatrix;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -19,6 +20,9 @@ import static java.lang.Math.*;
 
 public class MatrixUtilities {
     private static final Random random = new Random();
+    private static int randSize() {
+        return 10 + abs(random.nextInt()) % 91;
+    }
     public static double[][] generateMatrix() {
         int n = abs(random.nextInt()) % 100;
         double [][] matrix = new double[n][n];
@@ -41,7 +45,7 @@ public class MatrixUtilities {
         return el[abs(random.nextInt()) % 5];
     }
     private static double[][] generateAk(int k) {
-        int n = 10 + abs(random.nextInt()) % 91;
+        int n = randSize();
         double[][] res = new double[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -53,6 +57,16 @@ public class MatrixUtilities {
             res[i][i] = -Arrays.stream(res[i]).sum();
         }
         res[0][0] += Math.pow(10, -k);
+        return res;
+    }
+    private static double[][] generateGilbert() {
+        int n = randSize();
+        double[][] res = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i][j] = 1.0/(i + j + 1);
+            }
+        }
         return res;
     }
     private static double[] generateX(int n) {
@@ -73,13 +87,12 @@ public class MatrixUtilities {
     public static String vToString(double[] v) {
         return Arrays.stream(v).mapToObj(Double::toString).collect(Collectors.joining(" ", "", "\n"));
     }
-    public static void genWriteAkSoLE(String fileName, int k) {
-        double[][] ak = generateAk(k);
-        int n = ak.length;
+    private static void genWriteSoLE(String fileName, double[][] m) {
+        int n = m.length;
         double[] x = generateX(n);
-        double[] f = multMatrVect(ak, x);
-        ProfileMatrix m = new ProfileMatrix(ak);
-        m.writeInFile(fileName);
+        double[] f = multMatrVect(m, x);
+        ProfileMatrix matrix = new ProfileMatrix(m);
+        matrix.writeInFile(fileName);
         try (BufferedWriter writer = Files.newBufferedWriter(Path.of(fileName), StandardOpenOption.APPEND)) {
             writer.write(vToString(f));
             writer.write(vToString(x));
@@ -87,8 +100,28 @@ public class MatrixUtilities {
             e.printStackTrace();
         }
     }
+    public static void genWriteAkSoLE(String fileName, int k) {
+        genWriteSoLE(fileName, generateAk(k));
+    }
+    public static void genWriteGilbertSoLE(String fileName, int k) {
+        genWriteSoLE(fileName, generateGilbert());
+    }
     public static void genWriteMatrix(String fileName) {
         ProfileMatrix a = new ProfileMatrix(generateMatrix());
         a.writeInFile(fileName);
+    }
+    public static double[] readLineVector(BufferedReader reader) {
+        try {
+            return Arrays.stream(reader.readLine().split("\\s+")).filter(x -> !x.isEmpty()).mapToDouble(Double::valueOf).toArray();
+        } catch (IOException ignored) {
+        }
+        return new double[0];
+    }
+    public static double dist(double[] v1, double[] v2) {
+        double result = 0;
+        for (int i = 0; i < v1.length; i++) {
+            result += Math.pow(v1[i] - v2[i], 2);
+        }
+        return sqrt(result);
     }
 }
---
+++ src/thirdLab/Point23.java
@@ -0,0 +1,58 @@
+package thirdLab;
+
+import thirdLab.matrix.ProfileMatrix;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.lang.reflect.Method;
+
+public class Point23 {
+    final static int TESTCOUNT = 10;
+    private static void test(String dir, Method method) {
+        try {
+            if (Path.of(dir).getParent() != null) {
+                Files.createDirectories(Path.of(dir));
+            }
+            for (int i = 0; i < TESTCOUNT; i++) {
+                int k = i + 1;
+                String curTest = dir + File.separator + "test_" + k + ".txt";
+                method.invoke(null, curTest, k);
+                ProfileMatrix m = new ProfileMatrix(curTest);
+                try (BufferedReader reader = Files.newBufferedReader(Path.of(curTest))) {
+                    for (int j = 0; j < 4; j++) {
+                        reader.readLine();
+                    }
+                    double[] f = MatrixUtilities.readLineVector(reader);
+                    printRes(m.getColumnNumbers(), k, MatrixUtilities.readLineVector(reader), new LUMethod().solve(m, f));
+                }
+            }
+        } catch (IOException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+    private static Method findMethodByName(String name) {
+        Method[] methods = MatrixUtilities.class.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().equals(name)) {
+                return method;
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        test("matr/point2", findMethodByName("genWriteAkSoLE"));
+        test("matr/point3", findMethodByName("genWriteGilbertSoLE"));
+    }
+    private static void printRes(int n, int k, double[] x, double[] xk) {
+        double t = MatrixUtilities.dist(x, xk);
+        double xm = MatrixUtilities.dist(x, new double[x.length]);
+        if (xm != 0) {
+            xm = t/xm;
+        }
+        System.out.println(n + " " + k + " " + t + " " + xm);
+    }
+}
