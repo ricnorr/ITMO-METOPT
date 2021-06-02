7d61c9c5321eaf91c90bc166c601f6555a1964be
nikolai <f>
2021-06-03 00:42:46 +0300

    hz

--- src/fourthLab/method/Functions.java
+++ src/fourthLab/method/Functions.java
@@ -1,33 +1,99 @@
 package fourthLab.method;
 
-import fourthLab.research.Test;
-import secondLab.method.DescentGradientMethod;
-
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Functions {
     //f(x) = 100(y − x^2 )^2 + (1 − x)^2
-    public static Function<double[], Double> function1 = x -> Math.pow(100 * (x[1] - x[0]*x[0]), 2) + Math.pow(1 - x[0], 2);
+    public static Function<double[], Double> function1 = x -> 100 * Math.pow((x[1] - x[0] * x[0]), 2) + Math.pow(1 - x[0], 2);
     public static BiFunction<Integer, double[], Double> derivative1 = (a, b) -> {
-       switch (a) {
-           case 0:
-               return 2 * (200 * Math.pow(b[0], 3) - 200 * b[0] * b[1] + b[0] - 1);
-           case 1:
-               return 200 * (b[1] - Math.pow(b[0], 2));
-           default:
-               throw new RuntimeException("No such coord in derivative");
-       }
+        switch (a) {
+            case 0:
+                return 2 * (200 * Math.pow(b[0], 3) - 200 * b[0] * b[1] + b[0] - 1);
+            case 1:
+                return 200 * (b[1] - Math.pow(b[0], 2));
+            default:
+                throw new RuntimeException("No such coord in derivative");
+        }
+    };
+
+    //(x*x + y - 11)^2 +(x + y * y - 7)^2
+    public static Function<double[], Double> function2 = x -> Math.pow(x[0] * x[0] + x[1] - 11, 2) + Math.pow(x[0] + x[1] * x[1] - 7, 2);
+    public static BiFunction<Integer, double[], Double> derivative2 = (a, b) -> {
+        switch (a) {
+            case 0:
+                return 2 * (-7 + b[0] + b[1] * b[1] + 2 * b[0] * (-11 + b[0] * b[0] + b[1]));
+            case 1:
+                return 2 * (b[0] * b[0] + 2 * b[1] * (b[0] + b[1] * b[1] - 7) + b[1] - 11);
+            default:
+                throw new RuntimeException("No such coord in derivative");
+        }
     };
 
 
+    //(a + 10*b)^2 + 5 * (c - d)^2 + (b - 2c)^4 + 10 * (a - d)^4
+    // (0, 0,0, 0)
+    public static Function<double[], Double> function3 = x -> Math.pow(x[0] + 10 * x[1], 2) + 5 * Math.pow(x[2] - x[3], 2) + Math.pow(x[1] - 2 * x[2], 4) + 10 * Math.pow(x[0] - x[3], 4);
+    public static BiFunction<Integer, double[], Double> derivative3 = (a, b) -> {
+        switch (a) {
+            case 0:
+                return 2 * (20 * Math.pow(b[0] - b[3], 3) + b[0] + 10 * b[1]);
+            case 1:
+                return 4 * (5 * (b[0] + 10 * b[1]) + Math.pow(b[1] - 2 * b[2], 3));
+            case 2:
+                return 10 * (b[2] - b[3]) - 8 * Math.pow(b[1] - 2 * b[2], 3);
+            case 3:
+                return 10 * (-4 * Math.pow(b[0] - b[3], 3) - b[2] + b[3]);
+            default:
+                throw new RuntimeException("КУДА ЛЕЗЕШЬ");
+        }
+    };
+
+    // 100 - 2 / (1 + ((x - 1)/2)^2 + ((y - 2)/3)^2) - 1 / (1 + ((x - 2)/2)^2 + ((y - 1)/3)^2)
+    // (1.27, 1.72)
+    public static Function<double[], Double> function4 = (a) -> 100 -
+            (2 / (1 + Math.pow((a[0] - 1) / 2, 2) + Math.pow((a[1] - 1) / 3, 2))) - (1 / (1 + Math.pow((a[0] - 2) / 2, 2) + Math.pow((a[1] - 1) / 3, 2)));
+    public static BiFunction<Integer, double[], Double> derivative4 = (a, b) -> {
+        switch (a) {
+            case 0:
+                return (b[0] - 2) / (2 * Math.pow(1.0 / 4 * Math.pow(b[0] - 2, 2) + 1.0 / 9 * Math.pow(b[1] - 1, 2) + 1, 2)) +
+                        (b[0] - 1) / Math.pow(1.0 / 4 * Math.pow(b[0] - 1, 2) + 1.0 / 9 * Math.pow(b[1] - 2, 2) + 1, 2);
+            case 1:
+                return (4 * (b[1] - 2)) / (9 * Math.pow(1.0 / 4 * Math.pow(b[0] - 1, 2) + 1.0 / 9 * Math.pow(b[1] - 2, 2) + 1, 2)) +
+                        2 * (b[1] - 1) / (9 *
+                                Math.pow(
+                                        1.0 / 4 * Math.pow(b[0] - 2, 2) + 1.0 / 9 * Math.pow(b[1] - 1, 2) + 1, 2));
+            default:
+                throw new RuntimeException("ТЫ КУДА ЗААЛЕЗ ДУРЕНЬ");
+        }
+    };
+
+    static List<Function<double[], Double>> bifunc = List.of(function1, function2, function4);
+    static List<BiFunction<Integer, double[], Double>> biderivatives = List.of(derivative1, derivative2, derivative4);
+
+
+    static List<Function<double[], Double>> fourfunc = List.of(function3);
+    static List<BiFunction<Integer, double[], Double>> fourderivatives = List.of(derivative3);
+
     public static void main(String[] args) {
-        Arrays.stream(new KvasiNewton().run(derivative1, function1, new double[]{2, 2})).forEach(System.out::println);
-        //new DescentGradientMethod().runImpl(function1, )
+        // а ещё у квазиньютоновских сверхлинейная скорость сходимости, да да!
+        //Arrays.stream(new KvasiNewton().runImpl(derivative2, function2, new double[]{30, 30})).forEach(System.out::println);
+        for (int i = 0; i < fourfunc.size(); i++) {
+            if (i != 0) {
+                continue;
+            }
+            double[] point = new double[]{-20, -20, -20, -20};
+            System.out.println("Function " + i);
+            for (int j = 0; j < 5; j++) {
+                double[] temp_point = new double[]{point[0] + 5 * j, point[1] + 5 * j, point[2] + 5 * j, point[3] + 5 * j};
+                System.out.println(temp_point[0]);
+                Arrays.stream(new KvasiNewton().runPauellPrintPoints(fourderivatives.get(i), fourfunc.get(i), point));
+                }
+            }
+        }
+
+
     }
 
-
-
-
-}
--- src/fourthLab/method/KvasiNewton.java
+++ src/fourthLab/method/KvasiNewton.java
@@ -1,9 +1,11 @@
 package fourthLab.method;
 
+import firstLab.method.BrentMethod;
 import firstLab.method.FibonacciMethod;
 import firstLab.method.GoldenRatioMethod;
 import firstLab.method.ParabolaMethod;
 
+import java.lang.reflect.Method;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -14,58 +16,127 @@ import java.util.function.Function;
 public class KvasiNewton extends AbstactNewtoneMethod {
 
     // in david fletcher mode
-    @Override
-    public double[] runImpl(BiFunction<Integer, double[], Double> derivative, Function<double[], Double> function, double[] point) {
+    public double[] runImplPrintKoef(BiFunction<Integer, double[], Double> derivative, Function<double[], Double> function, double[] point, double[] ans) {
         double[][] G_1 = generateI(point.length);
         double[] w_1 = getAntiGradient(derivative, point);
         double[] p_1 = getAntiGradient(derivative, point);
         double[] finalP_ = p_1;
-        double alpha_1 = new GoldenRatioMethod(alph -> function.apply(sumVectors(point, multVector(finalP_, alph)))).run(-1000, 1000, EPSILON);
+        double alpha_1 = new FibonacciMethod(alph -> function.apply(sumVectors(point, multVector(finalP_, alph)))).run(0, 2, 0.0000000000000000000000000000001);
         double[] x_1 = sumVectors(point, multVector(p_1, alpha_1));
         double[] delta_x_1 = subtract(x_1, point);
-
-
-        for (int i = 0; i < MAX_ITERATIONS; i++) {
+        //System.out.println(point[0] + " " + point[1]);
+        double mx = Double.MIN_VALUE;
+        int iterations = 1;
+        System.out.println(length(subtract(ans, x_1)) + "\\\\ \\hline");
+        for (int i = 0; i < 150; i++) {
             double[] w_k = getAntiGradient(derivative, x_1);
             double[] delta_w_k = subtract(w_k, w_1);
             double[] v_k = multMatrix(G_1, delta_w_k);
             double[][] G_k = getNextGDavid(G_1, delta_x_1, delta_w_k, v_k); //
             double[] p_k = multMatrix(G_k, w_k);
             double[] finalX_1 = x_1;
-            double alpha_k = new GoldenRatioMethod(alph -> function.apply(sumVectors(finalX_1, multVector(p_k, alph)))).run(-10000, 10000, EPSILON);
+            double alpha_k = new GoldenRatioMethod(alph -> function.apply(sumVectors(finalX_1, multVector(p_k, alph)))).run(-1, 1, 0.000000000000000000000000000001);
             double[] x_k = sumVectors(x_1, multVector(p_k, alpha_k));
+            System.out.println(length(subtract(ans, x_k)) + "\\\\ \\hline");
+            mx = Math.max(mx, length(subtract(ans, x_k)) / length(subtract(ans, x_1)));
             double[] delta_x_k = subtract(x_k, x_1);
             /////
-            if (length(delta_x_k) < EPSILON) {
+            if (length(delta_x_k) < 0.0000001) {
                 x_1 = x_k;
                 break;
             }
             /////
+            //System.out.println("Arrow[{" + x_1[0] + "," + x_1[1] + "},{" + x_k[0] + "," + x_k[1] + "}]");
             w_1 = w_k;
             x_1 = x_k;
             delta_x_1 = delta_x_k;
             G_1 = G_k;
+            iterations++;
         }
+        System.out.println("Max:" + mx);
         return x_1;
     }
 
-    // in pauell mode
-    public double[] runImplPauell(BiFunction<Integer, double[], Double> derivative, Function<double[], Double> function, double[] point) {
+
+    public double[] runImplPrintPoints(BiFunction<Integer, double[], Double> derivative, Function<double[], Double> function, double[] point) {
         double[][] G_1 = generateI(point.length);
         double[] w_1 = getAntiGradient(derivative, point);
         double[] p_1 = getAntiGradient(derivative, point);
-        double alpha_1 = new GoldenRatioMethod(alph -> function.apply(sumVectors(point, multVector(p_1, alph)))).run(-1000, 1000, EPSILON);
+        double[] finalP_ = p_1;
+        double alpha_1 = new FibonacciMethod(alph -> function.apply(sumVectors(point, multVector(finalP_, alph)))).run(0, 2, 0.0000000000000000000000000000001);
         double[] x_1 = sumVectors(point, multVector(p_1, alpha_1));
         double[] delta_x_1 = subtract(x_1, point);
+        //System.out.println(point[0] + " " + point[1]);
+        double mx = Double.MIN_VALUE;
+        int iterations = 1;
 
-        for (int i = 0; i < MAX_ITERATIONS; i++) {
+
+        /*for (double x : point) {
+            System.out.print(x + " ");
+        }
+        System.out.println();*/
+
+
+        /*for (double x : x_1) {
+            System.out.print(x + " ");
+        }
+        System.out.println();*/
+
+        for (int i = 0; i < 150; i++)   {
+            double[] w_k = getAntiGradient(derivative, x_1);
+            double[] delta_w_k = subtract(w_k, w_1);
+            double[] v_k = multMatrix(G_1, delta_w_k);
+            double[][] G_k = getNextGDavid(G_1, delta_x_1, delta_w_k, v_k); //
+            double[] p_k = multMatrix(G_k, w_k);
+            double[] finalX_1 = x_1;
+            double alpha_k = new GoldenRatioMethod(alph -> function.apply(sumVectors(finalX_1, multVector(p_k, alph)))).run(-1, 1, 0.000000000000000000000000000001);
+            double[] x_k = sumVectors(x_1, multVector(p_k, alpha_k));
+            mx = Math.max(mx, length(subtract(new double[]{1.27,1.72}, x_k)) / length(subtract(new double[]{1.27,1.72}, x_1)));
+            double[] delta_x_k = subtract(x_k, x_1);
+            if (length(delta_x_k) < 0.0000001) {
+                x_1 = x_k;
+                break;
+            }
+            w_1 = w_k;
+            x_1 = x_k;
+            delta_x_1 = delta_x_k;
+            G_1 = G_k;
+            iterations++;
+            /*for (double x : x_k) {
+                System.out.print(x + " ");
+            }
+            System.out.println();*/
+        }
+        System.out.println("Iterations :" + iterations);
+        return x_1;
+
+    }
+
+    //в in pauell mode
+    public double[] runPauellPrintPoints(BiFunction<Integer, double[], Double> derivative, Function<double[], Double> function, double[] point) {
+        double[][] G_1 = generateI(point.length);
+        double[] w_1 = getAntiGradient(derivative, point);
+        double[] p_1 = getAntiGradient(derivative, point);
+        double alpha_1 = new BrentMethod(alph -> function.apply(sumVectors(point, multVector(p_1, alph)))).run(-1, 1, 0.0000000000000000001);
+        double[] x_1 = sumVectors(point, multVector(p_1, alpha_1));
+        double[] delta_x_1 = subtract(x_1, point);
+        /* for (double x : x_1) {
+            System.out.print(x + " ");
+        }
+        System.out.println(); */
+        int iterations = 1;
+        for (int i = 0; i < 100; i++) {
+            /*for (double x : x_1) {
+                System.out.print(x + " ");
+            }
+            System.out.println();*/
             double[] w_k = getAntiGradient(derivative, x_1);
             double[] delta_w_k = subtract(w_k, w_1);
             double[] delta_x_1_wave = sumVectors(delta_x_1, multMatrix(G_1, delta_w_k));
             double[][] G_k = getNextGPauell(G_1, delta_x_1_wave, delta_w_k);
             double[] p_k = multMatrix(G_k, w_k);
             double[] finalX_1 = x_1;
-            double alpha_k = new GoldenRatioMethod(alph -> function.apply(sumVectors(finalX_1, multVector(p_k, alph)))).run(-1000, 1000, EPSILON);
+            double alpha_k = new BrentMethod(alph -> function.apply(sumVectors(finalX_1, multVector(p_k, alph)))).run(-1, 1, 0.00000000001);
             double[] x_k = sumVectors(x_1, multVector(p_k, alpha_k));
             double[] delta_x_k = subtract(x_k, x_1);
             /////
@@ -73,15 +144,61 @@ public class KvasiNewton extends AbstactNewtoneMethod {
                 x_1 = x_k;
                 break;
             }
-            /////
+            /*for (double x : x_k) {
+                System.out.print(x + " ");
+            }
+            System.out.println();*/
             w_1 = w_k;
             x_1 = x_k;
             delta_x_1 = delta_x_k;
             G_1 = G_k;
+            iterations++;
         }
+        System.out.println("Iterations: " + iterations);
+        //System.out.println(point[0] + " " + point[1]  + " & " + iterations + "\\\\ \\hline");
         return x_1;
     }
 
+
+    public double[] runPauellPrintKoeffc(BiFunction<Integer, double[], Double> derivative, Function<double[], Double> function, double[] point, double[] ans) {
+        double[][] G_1 = generateI(point.length);
+        double[] w_1 = getAntiGradient(derivative, point);
+        double[] p_1 = getAntiGradient(derivative, point);
+        double alpha_1 = new BrentMethod(alph -> function.apply(sumVectors(point, multVector(p_1, alph)))).run(-1, 1, 0.0000000000000000001);
+        double[] x_1 = sumVectors(point, multVector(p_1, alpha_1));
+        double[] delta_x_1 = subtract(x_1, point);
+
+
+        double mx = Double.MIN_VALUE;
+        int iterations = 1;
+        for (int i = 0; i < 100; i++) {
+            double[] w_k = getAntiGradient(derivative, x_1);
+            double[] delta_w_k = subtract(w_k, w_1);
+            double[] delta_x_1_wave = sumVectors(delta_x_1, multMatrix(G_1, delta_w_k));
+            double[][] G_k = getNextGPauell(G_1, delta_x_1_wave, delta_w_k);
+            double[] p_k = multMatrix(G_k, w_k);
+            double[] finalX_1 = x_1;
+            double alpha_k = new BrentMethod(alph -> function.apply(sumVectors(finalX_1, multVector(p_k, alph)))).run(-1, 1, 0.00000000001);
+            double[] x_k = sumVectors(x_1, multVector(p_k, alpha_k));
+            double[] delta_x_k = subtract(x_k, x_1);
+            /////
+            if (length(delta_x_k) < EPSILON) {
+                x_1 = x_k;
+                break;
+            }
+            mx = Math.max(mx, length(subtract(ans, x_k)) / length(subtract(ans, x_1)));
+
+
+            w_1 = w_k;
+            x_1 = x_k;
+            delta_x_1 = delta_x_k;
+            G_1 = G_k;
+            iterations++;
+        }
+        System.out.println("Max coeff :" + mx);
+        //System.out.println(point[0] + " " + point[1]  + " & " + iterations + "\\\\ \\hline");
+        return x_1;
+    }
 
 
 
@@ -122,6 +239,11 @@ public class KvasiNewton extends AbstactNewtoneMethod {
         return subtractMatrix(
                 subtractMatrix(Gk, part1),
                 part2);
+    }
+
+    @Override
+    protected double[] runImpl(BiFunction<Integer, double[], Double> derivative, Function<double[], Double> function, double[] point) {
+        return new double[0];
     }
 }
 
