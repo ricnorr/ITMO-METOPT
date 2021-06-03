ecdf40b5d4b045bd5e748f84bc692385e6b23999
Deimoz <53293198+Deimoz@users.noreply.github.com>
2021-06-03 22:36:43 +0300

    Little fix of cool newton

--- src/fourthLab/method/CoolNewtonMethod.java
+++ src/fourthLab/method/CoolNewtonMethod.java
@@ -19,22 +19,25 @@ public class CoolNewtonMethod extends AbstactNewtoneMethod{
     @Override
     protected double[] runImpl(Gradient gradient, Function<double[], Double> function, double[] point) {
         double[] s, d, x = point.clone();
-        d = multVector(gradient.getGradient(x), -1);
+        System.out.println(x[0] + " " + x[1]);
+        /*d = multVector(gradient.getGradient(x), -1);
         s = multVector(d, findArgMinGolden(x, d, function));
-        x = sumVectors(x, s);
+        x = sumVectors(x, s);*/
         int i = 0;
+        //System.out.println(x[0] + " " + x[1]);
         for (int iter = 0; iter < MAX_ITERATIONS; iter++) {
             i++;
             double[] grad = gradient.getGradient(x);
             double[] antiGradient = multVector(grad, -1);
             s = new thirdLab.method.GaussMethod().solve(new StandardMatrix(H.evaluate(x)), antiGradient);
-            if (multVectors(s, grad) - EPSILON < 0) {
+            if (multVectors(s, grad) < 0) {
                 d = s.clone();
             } else {
                 d = antiGradient.clone();
             }
             s = multVector(d, findArgMinGolden(x, d, function));
             x = sumVectors(x, s);
+            System.out.println(x[0] + " " + x[1]);
             if (len(s) <= EPSILON) {
                 break;
             }
