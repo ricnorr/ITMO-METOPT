17e762ea633186adac4a95dfa1d3d29b1f30b6f9
Deimoz <53293198+Deimoz@users.noreply.github.com>
2021-06-03 17:00:36 +0300

    Update of Newton's methods

--- src/fourthLab/method/CoolNewtonMethod.java
+++ src/fourthLab/method/CoolNewtonMethod.java
@@ -22,15 +22,16 @@ public class CoolNewtonMethod extends AbstactNewtoneMethod{
         d = multVector(gradient.getGradient(x), -1);
         s = multVector(d, findArgMinGolden(x, d, function));
         x = sumVectors(x, s);
+        int i = 0;
         for (int iter = 0; iter < MAX_ITERATIONS; iter++) {
+            i++;
             double[] grad = gradient.getGradient(x);
             double[] antiGradient = multVector(grad, -1);
-            // s задает направление спуска
             s = new thirdLab.method.GaussMethod().solve(new StandardMatrix(H.evaluate(x)), antiGradient);
-            if (multVectors(s, grad) < 0) {
-                d = s;
+            if (multVectors(s, grad) - EPSILON < 0) {
+                d = s.clone();
             } else {
-                d = antiGradient;
+                d = antiGradient.clone();
             }
             s = multVector(d, findArgMinGolden(x, d, function));
             x = sumVectors(x, s);
@@ -38,12 +39,7 @@ public class CoolNewtonMethod extends AbstactNewtoneMethod{
                 break;
             }
         }
+        System.out.println(i);
         return x;
     }
-
-    private double findArgMinGolden(double[] point, double[] d, Function<double[], Double> function) {
-        return new GoldenRatioMethod(x -> function.apply(sumVectors(point, multVector(d, x))))
-                .run(-100, 100, 0.00001);
-    }
-
 }
